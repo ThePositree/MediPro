a9a3500754ac6cf98965aa1defff072138b1f166
ThePositree <ThePositree@yandex.ru>
2024-10-19 00:28:15 +0300

    feat: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-function App() {
+import { Link } from "react-router-dom"
+
+const App = () => {
   return (
+    <>
     <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    <Link className="text-blue-500 hover:text-blue-600" to="/catalog">Catalog</Link>
+    </>
   )
 }
 
